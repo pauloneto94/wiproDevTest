d9cd602f146ace4eb2be7f8d139da4107209d613
pauloneto94 <pauloneto913@gmail.com>
2020-04-30 16:54:53 -0300

    first commit
    Create project with dependecies

---
+++ src/main/java/com/pauloNeto/wipro/BancoOriginalTesteDevApplication.java
@@ -0,0 +1,13 @@
+package com.pauloNeto.wipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoOriginalTesteDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoOriginalTesteDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pauloNeto/wipro/BancoOriginalTesteDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloNeto.wipro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoOriginalTesteDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
